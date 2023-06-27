8bf8ce8d56f9f24cf73288f73655806d70602f8f
Lukas Kalvelage <lukas.kalvelage@student.jade-hs.de>
2023-06-27 18:51:05 +0200

    Added wiringPi to vcs

--- main.c
+++ main.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <wiringPi.h>
 
-int main (void)
-{
-    wiringPiSetup () ;
-    pinMode (0, OUTPUT) ;
-    for (;;)
-    {
-        digitalWrite (0, HIGH) ; delay (500) ;
-        digitalWrite (0,  LOW) ; delay (500) ;
+#define MAXTIMINGS    85
+#define DHTPIN        6
+
+int dht11_dat[5] = {0, 0, 0, 0, 0};
+
+
+
+/**
+ * https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
+ */
+void read_dht11_dat() {
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    float f;
+
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+    pinMode(DHTPIN, OUTPUT);
+    digitalWrite(DHTPIN, LOW);
+    delay(18);
+    digitalWrite(DHTPIN, HIGH);
+    delayMicroseconds(40);
+    pinMode(DHTPIN, INPUT);
+
+    for (i = 0; i < MAXTIMINGS; i++) {
+        counter = 0;
+        while (digitalRead(DHTPIN) == laststate) {
+            counter++;
+            delayMicroseconds(1);
+            if (counter == 255) {
+                break;
+            }
+        }
+        laststate = digitalRead(DHTPIN);
+
+        if (counter == 255)
+            break;
+        if ((i >= 4) && (i % 2 == 0)) {
+            dht11_dat[j / 8] <<= 1;
+            if (counter > 16)
+                dht11_dat[j / 8] |= 1;
+            j++;
+        }
     }
-    return 0 ;
+    if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF))) {
+        f = dht11_dat[2] * 9. / 5. + 32;
+        printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+               dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+    } else {
+        printf("Data not good, skip\n");
+    }
+}
+
+
+int main(void) {
+    wiringPiSetup();
+    pinMode(0, OUTPUT);
+
+    for (;;) {
+        digitalWrite(0, HIGH);
+        delay(500);
+        digitalWrite(0, LOW);
+        delay(500);
+    }
+    return 0;
 }
