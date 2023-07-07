2bce7ab0e396514f7eca25c8d778ac0c9758b418
Lukas Kalvelage <lukas.kalvelage@student.jade-hs.de>
2023-07-07 20:13:04 +0200

    restruct main.c

--- main.c
+++ main.c
@@ -1,72 +1,252 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <wiringPi.h>
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 
-#define MAXTIMINGS    85
-#define DHTPIN        6
+typedef struct {
+    double temp;
+    double airhum;
+    double grdhum;
+    double brightness;
+    double alert;
+} sensorData;
 
-int dht11_dat[5] = {0, 0, 0, 0, 0};
+void getSensorValues() {
 
+}
 
-
-/**
- * https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
- */
-void read_dht11_dat() {
-    uint8_t laststate = HIGH;
-    uint8_t counter = 0;
-    uint8_t j = 0, i;
-    float f;
-
-    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
-
-    pinMode(DHTPIN, OUTPUT);
-    digitalWrite(DHTPIN, LOW);
-    delay(18);
-    digitalWrite(DHTPIN, HIGH);
-    delayMicroseconds(40);
-    pinMode(DHTPIN, INPUT);
-
-    for (i = 0; i < MAXTIMINGS; i++) {
-        counter = 0;
-        while (digitalRead(DHTPIN) == laststate) {
-            counter++;
-            delayMicroseconds(1);
-            if (counter == 255) {
-                break;
-            }
+void importData()
+{
+    system("@cls||clear");
+    printf("\n---------------------------\n| Messstation Blumentopf! |\n---------------------------\n\n");
+    for (;;)
+    {
+        int value = backToMenu();
+        if (value == 1)
+        {
+            /* return to Mainmenu */
+            return 0;
         }
-        laststate = digitalRead(DHTPIN);
-
-        if (counter == 255)
-            break;
-        if ((i >= 4) && (i % 2 == 0)) {
-            dht11_dat[j / 8] <<= 1;
-            if (counter > 16)
-                dht11_dat[j / 8] |= 1;
-            j++;
+        else
+        {
+            /* Print Values */
         }
-    }
-    if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF))) {
-        f = dht11_dat[2] * 9. / 5. + 32;
-        printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-               dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
-    } else {
-        printf("Data not good, skip\n");
     }
 }
 
+void exportData()
+{
+    char filename[128];
+    system("@cls||clear");
+    printf("\n---------------------------\n| Messstation Blumentopf! |\n---------------------------\n\n");
+    for (;;)
+    {
+        int value = backToMenu();
+        if (value == 1)
+        {
+            /* return to Mainmenu */
+            return 0;
+        }
+        else
+        {
+            /* Print Values */
+            printf("Dateinamen waehlen!\n");
+            fflush(stdin);
+            scanf("%s", filename);
+            strcat(filename, ".txt");
 
-int main(void) {
-    wiringPiSetup();
-    pinMode(0, OUTPUT);
-
-    for (;;) {
-        digitalWrite(0, HIGH);
-        delay(500);
-        digitalWrite(0, LOW);
-        delay(500);
+            /* 	FILE *fptr;
+            fptr = fopen("filename.txt", "w"); */
+        }
     }
+}
+
+void settings() {
+    system("@cls||clear");
+    printf("\n---------------------------\n| Messstation Blumentopf! |\n---------------------------\n\n");
+    for (;;) {
+        int value = backToMenu();
+        if (value == 1) {
+            /* return to Mainmenu */
+            return 0;
+        } else {
+            /* shwo Settings */
+        }
+    }
+}
+
+void dataConsoleOutput(temp, airhum, grdhum, occupacy, alert) { // Funktion um Daten in Tabellarischer Form auszugeben
+
+    printf("|  Temperatur   | Luftfeuchtigkeit | Bodenfeuchtigkeit |   Helligkeit  | Alarm |");
+    printf("|%s|%s|%s|%s|%s|", &temp, &airhum, &grdhum, &occupacy, &alert);
+}
+
+void formatValues(double value, char *result) { // Funktion um Double Daten in Formatierten String umzuwandeln
+    snprintf(result, 21, "%f", value);
+    int len = strlen(result);
+    for (int i = len; i < 20; i++) {
+        result[i] = ' ';
+    }
+    result[20] = '\0';
+}
+
+void parseStringToStruct(const char *input, sensorData *data, int maxCount) {
+    char *inputCopy = strdup(input);
+    char *token = strtok(inputCopy, ";");
+
+    int count = 0;
+    while (token != NULL && count < maxCount) {
+        switch (count) {
+            case 0:
+                data->temp = atof(token);
+                break;
+            case 1:
+                data->airhum = atof(token);
+                break;
+            case 2:
+                data->grdhum = atof(token);
+                break;
+            case 3:
+                data->brightness = atof(token);
+                break;
+            case 4:
+                data->alert = atof(token);
+                break;
+        }
+
+        token = strtok(NULL, ";");
+        count++;
+    }
+
+    free(inputCopy);
+}
+
+int readDataOutput() {
+    char c;
+    char sensorValues;
+
+    system("@cls||clear");
+    printf("\n---------------------------\n| Messstation Blumentopf! |\n---------------------------\n\n");
+    for (;;) {
+        int value = backToMenu();
+        if (value == 1) {
+            /* return to Mainmenu */
+            return 0;
+        } else {
+            sensorData data;
+            // Hier sensordaten empfangen
+            sensorValues = getSensorValues();
+            /* Print Values */
+            parseStringToStruct(sensorValues, &data, 1);
+
+            // Ausgabe der gemessenen Werte
+            printf("Temperature: %.2f\n", data.temp);
+            printf("Air Humidity: %.2f\n", data.airhum);
+            printf("Ground Humidity: %.2f\n", data.grdhum);
+            printf("Brightness: %.2f\n", data.brightness);
+            printf("Alert: %.2f\n", data.alert);
+        }
+    }
+}
+
+int backToMenu() {
+    char myChar;
+    int validInput;
+    if (kbhit()) {
+        printf("Zurueck zum Hauptmenu?  y/n \n");
+        fflush(stdin);
+        do {
+            scanf("%c", &myChar);
+            switch (myChar) {
+                case 'y':
+                    validInput = 1;
+                    return 1;
+                    readDataOutput();
+                    break;
+                case 'n':
+                    validInput = 1;
+                    return 0;
+                    break;
+                default:
+                    printf("Falsche Eingabe\n");
+                    validInput = 0;
+                    break;
+            }
+        } while (validInput == 0);
+    }
+}
+
+void mainMenu() { // Funktion um das Hauptmenu in der Konsole auszuführen
+    char myChar;
+    int validInput;
+    system("@cls||clear");
+    printf("\n---------------------------\n| Messstation Blumentopf! |\n---------------------------\n\n");
+    printf("\n\nHauptmenu\n");
+    printf("1. Messung\n");
+    printf("2. Messung importieren\n");
+    printf("3. Messung exportieren\n");
+    printf("4. Einstellungen\n");
+    printf("5. Programm beenden\n\n");
+    fflush(stdin);
+    do
+    {
+        scanf("%c", &myChar);
+        switch (myChar)
+        {
+            case '1':
+                printf("Messung\n");
+                validInput = 1;
+                readDataOutput();
+                break;
+            case '2':
+                printf("importieren\n");
+                validInput = 1;
+                importData();
+                break;
+            case '3':
+                printf("exportieren\n");
+                validInput = 1;
+                exportData();
+                break;
+            case '4':
+                printf("Einstellungen\n");
+                validInput = 1;
+                settings();
+                break;
+            case '5':
+                validInput = 1;
+                _Exit(0);
+                break;
+            default:
+                printf("Falsche Eingabe\n");
+                validInput = 0;
+                break;
+        }
+    } while (validInput == 0);
+}
+
+int main() {
+    int serial_port = open("/dev/ttyACM0", O_RDWR);  // Serielle Schnittstelle öffnen (passen Sie den Pfad entsprechend an)
+
+    struct termios tty;
+    memset(&tty, 0, sizeof(tty));
+
+    // Serielle Schnittstellenkonfiguration
+    tty.c_cflag = B9600;  // Baudrate
+    tty.c_cflag |= CS8;   // 8 Datenbits
+    tty.c_cflag |= CLOCAL | CREAD;  // Lokale Verbindung, Empfang aktivieren
+
+    tcsetattr(serial_port, TCSANOW, &tty);  // Konfiguration anwenden
+
+    char buf[255];
+
+    for (;;)
+    {
+        mainMenu();
+    }
+
     return 0;
 }
